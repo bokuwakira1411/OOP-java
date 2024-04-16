6a58c6b2b8329e136ed0398dd9e864d376d5901f
bokuwakira1411@outlook.com <bokuwakira1411@outlook.com>
2024-04-16 15:55:22 +0700

    hi

---
+++ src/HW1/username.java
@@ -0,0 +1,15 @@
+package HW1;
+public class username {
+    public static void main(String[] args) {
+        System.out.print("Họ tên: Nguyễn Thị Ngọc Lan\t");
+        System.out.print("Mã sinh viên: 23020390\t");
+        System.out.print("Lớp: K68-AI2\t");
+        System.out.print("Username: bokuwakira1411\t");
+        System.out.println("Địa chỉ email: 23020390@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
